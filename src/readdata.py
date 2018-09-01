70c9970f1c28ce1125adc2c6656059b1bf51125b
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-31 20:51:55 -0500

    reformated code

--- src/readdata.py
+++ src/readdata.py
@@ -1,17 +1,50 @@
 import pandas as pd
+import numpy as np
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 
 df = pd.read_csv(url, header = None)
 df.head(5)
 df.tail(5)
 
+##Defining headers for the DataSet
 headers=["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels",
         "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-type", "num-of-cylinders",
          "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg",
          "highway-mpg", "price"]
 
 df.columns = headers
-#path = "/Users/ShreyamDuttaGupta/Desktop/automobile-data-set-analysis/cars.csv"
-#df.to_csv(path)
+
+##Exporting to csv
+"""
+
+path = "/Users/ShreyamDuttaGupta/Desktop/automobile-data-set-analysis/cars.csv"
+df.to_csv(path)
+
+"""
+
+##Generating descriptive stats
+"""
+
 #print(df.describe(include="all"))
-print(df.info)
+#print(df.info)
+
+"""
+
+##Data Formatting, replacing ? to NAN
+"""
+
+df["price"].replace('?',np.nan, inplace = True)
+path = "/Users/ShreyamDuttaGupta/Desktop/automobile-data-set-analysis/cars.csv"
+df.to_csv(path)
+
+"""
+
+##Data Formatting, converting prices from Object to Int, dropping NaN values
+"""
+
+df["price"].replace('?',np.nan, inplace = True)
+df.dropna(subset=["price"], axis=0, inplace=True)
+df["price"] = df["price"].astype("int")
+print(df.dtypes)
+
+"""
