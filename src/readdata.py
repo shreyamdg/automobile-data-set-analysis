c77fbb65870b55de193e3546470e4aea6717844f
shreyam15 <shreyam.duttagupta@gmail.com>
2018-09-01 00:59:49 -0500

    exploratory data analysis

--- src/readdata.py
+++ src/readdata.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 
@@ -53,6 +54,7 @@ df["price"] = df["price"].astype("int")
 
 
 ###Data Binning
+'''
 
 binwidth = int((max(df["price"])-min(df["price"]))/3)
 bins = range(min(df["price"]), max(df["price"]), binwidth)
@@ -62,10 +64,55 @@ path = "/Users/ShreyamDuttaGupta/Desktop/automobile-data-set-analysis/cars.csv"
 df.to_csv(path)
 df.dropna(subset=["price-binned"], axis=0, inplace=True)
 
+'''
+
 ##Plotting Histogram from the binned value
+'''
 
 plt.hist(df["price"],bins=3)
 plt.title("Price Bins")
 plt.xlabel("Count")
 plt.ylabel("Price")
 plt.show()
+
+'''
+
+#TURNING CATEGORICAL VARIABLES INTO QUANTITATIVE VARIABLES
+'''
+
+df = (pd.get_dummies(df["fuel-type"]))
+
+'''
+
+#DESCRIPTIVE STATISTICS- Value_counts
+'''
+
+drive_wheels_counts = df["drive-wheels"].value_counts()
+drive_wheels_counts.rename(columns={'drive-wheels':'value_counts'}, inplace=True)
+drive_wheels_counts.index.name = 'drive-wheels'
+print(drive_wheels_counts)
+
+'''
+
+#Box Plots
+'''
+
+sns.boxplot(x="drive-wheels", y="price", data=df)
+plt.show()
+
+'''
+
+#Scatterplot
+'''
+
+y=df["engine-size"]
+x=df["price"]
+plt.scatter(x,y)
+
+plt.title("Scatterplot of Engine Size vs Price")
+plt.xlabel("Engine Size")
+plt.ylabel
+("Price")
+plt.show()
+
+'''
